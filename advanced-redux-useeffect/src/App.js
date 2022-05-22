41f4f5d37894969805b97ae9a4748703c8207611
happydtd <happydtdau@yahoo.com.au>
2022-05-22 20:19:44 +1000

    finial

--- advanced-redux-useeffect/src/App.js
+++ advanced-redux-useeffect/src/App.js
@@ -1,18 +1,89 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+//import { uiActions } from './store/ui-slice';
+import Notification from '../src/components/UI/Notification'
 
-import {useSelector} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
+import {useEffect} from 'react'
+import { SendCartData, fetchCartData } from './store/cart-actions';
+
+let isInitial = true;
 
 function App() {
-
+  const dispatch = useDispatch();
   const showCart = useSelector(state=>state.ui.cartIsVisible);
+  const cart = useSelector(state=>state.cart);
+  const notification = useSelector(state=>state.ui.notification);
+
+  //使用useEffect去同步数据库
+  // useEffect(()=>{
+  //   console.log('call sendCartData')
+  //   const sendCartData = async ()=>{
+  //     dispatch(
+  //       uiActions.showNotification({
+  //         status: 'pending',
+  //         title:'Sending...',
+  //         message:'Sending cart data!',
+  //       })
+  //     )
+  //     const response = await fetch('https://react-http-5a2ef-default-rtdb.firebaseio.com/cartjson', {method: 'PUT', body: JSON.stringify(cart)});
+  //     if (!response.ok){
+  //       throw new Error('Sending cart data failed.')
+  //     }
+  //     //const responseData = await response.json();
+  //     dispatch(
+  //       uiActions.showNotification({
+  //         status: 'success',
+  //         title:'Success!',
+  //         message:'Send cart data successfully!',
+  //       })
+  //     )
+      
+  //   }
+    
+  //   if (isInitial) {
+  //     isInitial = false;
+  //     return;
+  //   }
+
+  //   sendCartData().catch(error=>{
+  //     dispatch(
+  //       uiActions.showNotification({
+  //         status: 'error',
+  //         title:'Error!',
+  //         message:'Send cart data failed!',
+  //       })
+  //     )
+  //   })
+  // },[cart, dispatch])
+  
+  useEffect(()=>{
+    dispatch(fetchCartData());
+  },[dispatch])
+
+  useEffect(()=>{
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    if (cart.changed){
+      //dispatch一个function里会return另一个function
+      dispatch(SendCartData(cart));
+    }
+
+  },[cart, dispatch])
 
   return (
-    <Layout>
-      {showCart &&<Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && <Notification status = {notification.status} title={notification.title} message={notification.message}/>}
+      <Layout>
+        {showCart &&<Cart />}
+        <Products />
+      </Layout>
+    </>
+
   );
 }
 
--- advanced-redux-useeffect/src/store/cart-slice.js
+++ advanced-redux-useeffect/src/store/cart-slice.js
@@ -1,16 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+
 const cartSlice = createSlice({
     name: 'cart',
     initialState : {
         items:[],
         totalQuantity:0,
+        changed: false
     },
     reducers : {
+        replaceCart(state, action){
+            state.totalQuantity = action.payload.totalQuantity;
+            state.items = action.payload.items;
+        },
         addItemToCart(state, action){
             const newItem = action.payload;
             const existingItem = state.items.find(item=>item.id === newItem.id);
             state.totalQuantity ++;
+            state.changed = true;
             if(!existingItem){
                 state.items.push({id: newItem.id, price: newItem.price, quantity: 1, totalPrice: newItem.price, name: newItem.title}); 
             }
@@ -23,6 +30,7 @@ const cartSlice = createSlice({
             const id = action.payload;
             const existingItem = state.items.find(item=>item.id === id)
             state.totalQuantity --;
+            state.changed = true;
             if (existingItem.quantity === 1){
                 state.items = state.items.filter(item=> item.id !== id);
             } else{
@@ -32,6 +40,8 @@ const cartSlice = createSlice({
         }
     }
 })
+
+
 
 export const cartActions = cartSlice.actions;
 
